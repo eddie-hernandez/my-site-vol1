08ab1ea26a35f19e7fe9038d0fba366aeafc4506
eddie-hernandez <71785228+eddie-hernandez@users.noreply.github.com>
2023-03-22 17:04:39 -0400

    fixing responsive design

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './Pages/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Router>
       <App />
-    </Router>
   </React.StrictMode>
 )
